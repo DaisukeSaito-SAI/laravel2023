17aca5583f9d14c9cecc467891db1ee686e10eab
test <rlststory@gmail.com>
2023-05-14 08:13:42 +0900

    test2

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
 
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
+
+Route::post('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId', '[0-9]+');
+Route::post('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
+
+Route::delete('/tweet/delete/{tweetId}', \App\Htpp\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
