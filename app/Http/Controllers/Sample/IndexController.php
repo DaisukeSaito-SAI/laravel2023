3a2e47ccaf1b97aa260c8897a0ad5b43343f3a1b
test <rlststory@gmail.com>
2023-05-14 06:05:27 +0900

    29230514 80p la9 book

---
+++ app/Http/Controllers/Sample/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Sample;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function show()
+    {
+        return 'へろー';
+    }
+
+
+    public function showID($id)
+    {
+        return "へろー {$id}";
+    }
+}
